9c25712418775b38c512731ffea20cc6ff9ca323
Lucas Soares <polonio230@hotmail.com>
2023-09-22 13:46:48 -0300

    code: add imprimir() and ler() methods

--- POO/src/Main.java
+++ POO/src/Main.java
@@ -1,5 +1,11 @@
+import entities.Jogador;
+
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá");
+
+        Jogador.ler();
     }
+
 }
--- POO/src/entities/Jogador.java
+++ POO/src/entities/Jogador.java
@@ -1,5 +1,6 @@
 package entities;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Jogador {
@@ -10,6 +11,8 @@ public class Jogador {
     private String universidade;
     private int anoNascimento;
     private String cidadeNascimento;
+
+    private String estado;
 
     public Jogador() {
 
@@ -29,33 +32,78 @@ public class Jogador {
 
     }*/
 
-    public void ler() {
-        Scanner sc = new Scanner(System.in);
-        String linha = sc.nextLine();
 
-        String[] palavras;
+    public static void ler() {
+        Scanner sc = new Scanner(System.in);
+        int tamanhoArray = 10;
+        Jogador[] jogadores = new Jogador[tamanhoArray];
         int i = 0;
+
+        String linha = sc.nextLine();
         while(sc.hasNextLine() && !linha.equals("FIM")) {
+            String[] palavras = linha.split(",");
+            if(i >= tamanhoArray) {
+                tamanhoArray *= 2;
+                Jogador[] novoArray = new Jogador[tamanhoArray];
+                System.arraycopy(jogadores, 0, novoArray, 0, jogadores.length);
+                jogadores = novoArray;
+            }
+            Jogador jogador = new Jogador();
+            jogadores[i] = jogador;
+
+            jogador.setId(palavras.length > 0 && !palavras[0].isEmpty() ? Integer.parseInt(palavras[0]) : 0);
+            jogador.setNome(palavras.length > 1 ? palavras[1] : "");
+            jogador.setAltura(palavras.length > 2 && !palavras[2].isEmpty() ? Integer.parseInt(palavras[2]) : 0);
+            jogador.setPeso(palavras.length > 3 && !palavras[3].isEmpty() ? Integer.parseInt(palavras[3]) : 0);
+            jogador.setUniversidade(palavras.length > 4 ? palavras[4] : "");
+            jogador.setAnoNascimento(palavras.length > 5 && !palavras[5].isEmpty() ? Integer.parseInt(palavras[5]) : 0);
+            jogador.setCidadeNascimento(palavras.length > 6 ? palavras[6] : "");
+            jogador.setEstado(palavras.length > 7 ? palavras[7] : "");
             i++;
             linha = sc.nextLine();
-            palavras = linha.split(",");
         }
-
-        Jogador[] jogadores = new Jogador[i];
-        for(int j = 0; j<jogadores.length; j++) {
-
+        linha = sc.nextLine();
+        int qtdLinhas = Integer.parseInt(linha);
+        for (int j = 0; j < qtdLinhas; j++) {
+            linha = sc.nextLine();
+            int w = 0;
+            while (w < jogadores.length && (jogadores[w] == null || Integer.parseInt(linha) != jogadores[w].getId())) {
+                w++;
+            }
+            if (w < jogadores.length && jogadores[w] != null) {
+                imprimir(jogadores[w]);
+            } else {
+                System.out.println("Jogador não encontrado para o ID informado");
+            }
         }
-
-
 
         sc.close();
     }
 
- /*   public void imprimir() {
+    public static void imprimir(Jogador jogador) {
+        System.out.print("[" + jogador.getId() + " ## ");
+        System.out.print((jogador.getNome() == "" ? "nao informado" : jogador.getNome()) + " ## ");
+        System.out.print((jogador.getAltura() == 0 ? "nao informado" : jogador.getAltura()) + " ## ");
+        System.out.print((jogador.getPeso() == 0 ? "nao informado" : jogador.getPeso()) + " ## ");
+        System.out.print((jogador.getAnoNascimento() == 0 ? "nao informado" : jogador.getAnoNascimento()) + " ## ");
+        System.out.print((Objects.equals(jogador.getUniversidade(), "") ? "nao informado" : jogador.getUniversidade()) + " ## ");
+        System.out.print((Objects.equals(jogador.getCidadeNascimento(), "") ? "nao informado" :
+                jogador.getCidadeNascimento()) + " " +
+                "## ");
+        System.out.print((Objects.equals(jogador.getEstado(), "") ? "nao informado" :
+                jogador.getEstado()) +
+                "]");
+        System.out.println();
+    }
 
-    }*/
 
+    public String getEstado() {
+        return estado;
+    }
 
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 
     public int getId() {
         return id;
